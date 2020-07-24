bea9f6aaab22a4723d9f318c9f5c182de4525a16
ibrahim AKBULUT <57018928+ixakblt@users.noreply.github.com>
2020-07-24 16:26:45 +0300

    Add files via upload

---
+++ FacebookNoToVCF.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#################################
+#   FacebookCcontactsto vcf  / ixakblt   #
+#################################
+from bs4 import BeautifulSoup
+
+dosya = open("fblist.html",encoding="utf-8")
+yazilacaknolar = open("ixrehberforfb.vcf","a",encoding="utf-8")
+soup = BeautifulSoup(dosya.read(),"lxml")
+
+kisiler = soup.find_all("div",attrs={"class":"pam _3-95 _2pi0 _2lej uiBoxWhite noborder"})
+
+for i in kisiler:
+    try:
+        isim = i.find("div",attrs={"class":"_3-96 _2pio _2lek _2lel"}).text
+        soyisim = i.find("div",attrs={"class":"_3hls"}).text
+        vcfscript = """
+BEGIN:VCARD
+VERSION:2.1
+N:{};;;
+FN:{}
+TEL;CELL:{}
+END:VCARD
+
+
+""".format(isim,isim,soyisim)
+        print(vcfscript)
+    except:
+        continue
+    yazilacaknolar.write(vcfscript)
+
+yazilacaknolar.close()
